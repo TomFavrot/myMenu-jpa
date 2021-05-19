8ce11f647fcd50399351dd06acca3a1c397ce7e5
Baptiste Dessandier <baptiste.dessandier@gmail.com>
2021-05-19 12:29:55 +0200

    Correction et Appli V1

--- myMenu-jpa/src/main/java/sopra/myMenu/Application.java
+++ myMenu-jpa/src/main/java/sopra/myMenu/Application.java
@@ -3,11 +3,32 @@ package sopra.myMenu;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import sopra.myMenu.repository.IAdresseRepository;
+import sopra.myMenu.repository.IAjustementQuantiteRepository;
+import sopra.myMenu.repository.IIngredientRepository;
+import sopra.myMenu.repository.IListeCourseRepository;
+import sopra.myMenu.repository.IMagasinIngredientRepository;
+import sopra.myMenu.repository.IMagasinRepository;
+import sopra.myMenu.repository.IPlatRepository;
+import sopra.myMenu.repository.IPreferenceAlimentaireRepository;
+import sopra.myMenu.repository.IRecetteRepository;
+import sopra.myMenu.repository.IUtilisateurRepository;
+
 public class Application {
 	
 	private static Application instance = null;
-
 	private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("formation-jpa");
+	
+	private final IAdresseRepository adresseRepo;
+	private final IAjustementQuantiteRepository ajustquantitRepo;
+	private final IIngredientRepository ingredientRepo;
+	private final IListeCourseRepository listecourseRepo;
+	private final IMagasinIngredientRepository magasiningredRepo;
+	private final IMagasinRepository magasinRepo;
+	private final IPlatRepository platRepo;
+	private final IPreferenceAlimentaireRepository preferencealimRepo;
+	private final IRecetteRepository recetteRepo;
+	private final IUtilisateurRepository utilisateurRepo;
 	
 	private Application() {
 
@@ -24,4 +45,46 @@ public class Application {
 	public EntityManagerFactory getEmf() {
 		return emf;
 	}
+
+	public IAdresseRepository getAdresseRepo() {
+		return adresseRepo;
+	}
+
+	public IAjustementQuantiteRepository getAjustquantitRepo() {
+		return ajustquantitRepo;
+	}
+
+	public IIngredientRepository getIngredientRepo() {
+		return ingredientRepo;
+	}
+
+	public IListeCourseRepository getListecourseRepo() {
+		return listecourseRepo;
+	}
+
+	public IMagasinIngredientRepository getMagasiningredRepo() {
+		return magasiningredRepo;
+	}
+
+	public IMagasinRepository getMagasinRepo() {
+		return magasinRepo;
+	}
+
+	public IPlatRepository getPlatRepo() {
+		return platRepo;
+	}
+
+	public IPreferenceAlimentaireRepository getPreferencealimRepo() {
+		return preferencealimRepo;
+	}
+
+	public IRecetteRepository getRecetteRepo() {
+		return recetteRepo;
+	}
+
+	public IUtilisateurRepository getUtilisateurRepo() {
+		return utilisateurRepo;
+	}
+	
+	
 }
--- myMenu-jpa/src/main/java/sopra/myMenu/repository/IListeCoursesRepository.java
+++
@@ -1,7 +0,0 @@
-package sopra.myMenu.repository;
-
-import sopra.myMenu.model.ListeCourse;
-
-public interface IListeCoursesRepository extends IRepository<ListeCourse,Long> {
-
-}
