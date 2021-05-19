e5dd42e0e54787bea5a9c5ddee82ec0708ce79e6
Favrot <tom@>
2021-05-19 14:17:36 +0200

    RepositoryJpa Menu, Planning, Repas, Objectif

---
+++ myMenu-jpa/src/main/java/sopra/myMenu/repository/jpa/MenuRepositoryJpa.java
@@ -0,0 +1,78 @@
+package sopra.myMenu.repository.jpa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import sopra.myMenu.Application;
+import sopra.myMenu.model.Menu;
+
+
+public class MenuRepositoryJpa {
+
+public List<Menu> findAll() {
+		
+		List<Menu> menus = new ArrayList<Menu>();
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			TypedQuery<Menu> query = em.createQuery("select m from Menu m", Menu.class);
+
+			menus = query.getResultList();
+
+			tx.commit();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return menus;
+	}
+
+	public Menu findById(Long id) {
+
+		Menu menu = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			menu = em.find(Menu.class, id);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return menu;
+	}
+}
---
+++ myMenu-jpa/src/main/java/sopra/myMenu/repository/jpa/PlanningRepositoryJpa.java
@@ -0,0 +1,77 @@
+package sopra.myMenu.repository.jpa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import sopra.myMenu.Application;
+import sopra.myMenu.model.Planning;
+
+public class PlanningRepositoryJpa {
+
+	public List<Planning> findAll() {
+		
+		List<Planning> plannings = new ArrayList<Planning>();
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			TypedQuery<Planning> query = em.createQuery("select p from Planning p", Planning.class);
+
+			plannings = query.getResultList();
+
+			tx.commit();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return plannings;
+	}
+
+	public Planning findById(Long id) {
+
+		Planning planning = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			planning = em.find(Planning.class, id);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return planning;
+	}
+}
