7f03e383db5136db2f65cc895e6f763cdb4d2f90
camille <c>
2021-05-24 14:29:20 +0200

    Test approfondis + requêtes supplémentaires pour MagasinIngredient

--- myMenu-jpa/src/main/java/sopra/myMenu/model/AjustementQuantite.java
+++ myMenu-jpa/src/main/java/sopra/myMenu/model/AjustementQuantite.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
@@ -20,7 +21,7 @@ public class AjustementQuantite {
 	private Float quantiteModifiee;
 	@OneToMany(mappedBy = "ajustementQuantite")
 	private List<ListeCourse> listeCourse=new ArrayList<ListeCourse>();
-	@OneToMany(mappedBy = "ajustementQuantite")
+	@OneToMany(mappedBy = "ajustementQuantite",fetch = FetchType.EAGER)
 	private List<Ingredient> ingredients=new ArrayList<Ingredient>();
 	
 	
