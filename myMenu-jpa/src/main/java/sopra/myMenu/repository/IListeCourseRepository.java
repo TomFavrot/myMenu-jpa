b557c8c296d0a99a852e76cabbfc274937dc65a0
Baptiste Dessandier <baptiste.dessandier@gmail.com>
2021-05-25 15:32:35 +0200

    commit

--- myMenu-jpa/src/main/java/sopra/myMenu/repository/IListeCourseRepository.java
+++ myMenu-jpa/src/main/java/sopra/myMenu/repository/IListeCourseRepository.java
@@ -1,7 +1,9 @@
 package sopra.myMenu.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import sopra.myMenu.model.ListeCourse;
 
-public interface IListeCourseRepository extends IRepository<ListeCourse,Long> {
+public interface IListeCourseRepository extends JpaRepository<ListeCourse,Long> {
 
 }
