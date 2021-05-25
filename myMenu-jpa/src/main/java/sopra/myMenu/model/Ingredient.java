df42bbf20ed64509908941cee3d9127d0fd784f4
Baptiste Dessandier <baptiste.dessandier@gmail.com>
2021-05-25 14:48:35 +0200

    commit projet spring init

--- myMenu-jpa/src/main/java/sopra/myMenu/model/Ingredient.java
+++ myMenu-jpa/src/main/java/sopra/myMenu/model/Ingredient.java
@@ -7,7 +7,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -15,7 +14,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 
 
