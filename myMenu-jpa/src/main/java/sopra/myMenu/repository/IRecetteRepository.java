4b4d9e316a6e6b47541b5447f6319d4d1aff7523
Solenn <delta@g.fr>
2021-06-06 12:59:58 +0200

    requetes supp recette et ingredients

--- myMenu-jpa/src/main/java/sopra/myMenu/repository/IRecetteRepository.java
+++ myMenu-jpa/src/main/java/sopra/myMenu/repository/IRecetteRepository.java
@@ -1,9 +1,24 @@
 package sopra.myMenu.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 import sopra.myMenu.model.Recette;
 
 public interface IRecetteRepository extends JpaRepository<Recette, Long>{
-
+	@Query("select r from Recette r where r.nom= :nom")
+	List<Recette> findByName(@Param("nom") String nom);
+	
+	@Query("select r from Recette r ORDER BY r.totalCalories ASC")
+	List<Recette> findByRisingCalories();
+	
+	@Query("select r from Recette r where r.note= :note")
+	List<Recette> findByNote(@Param("note") float note);
+	
+	@Query("select r from Recette r where r.type_alimentation= :type_alimentation")
+	List<Recette> findByTypeAlimentation(@Param("type_alimentation") String type_alimentation);
 }
